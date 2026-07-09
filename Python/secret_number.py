c6f377fe113f004c2a32baaabf7ea8ad0b140290
Krish Arora <223684188+krish373@users.noreply.github.com>
2026-07-09 16:40:06 +0530

    Improve number guessing game

--- Python/secret_number.py
+++ Python/secret_number.py
@@ -5,6 +5,10 @@ secret_number =  random.randint(1, 10)
 User_Guess = int(input("enter a number:"))
 
 while User_Guess != secret_number:
+    if User_Guess > secret_number:
+        print("Too high")
+    elif User_Guess < secret_number:
+        print("Too low")
     User_Guess = int(input("Enter a number:"))
 else:
-    print("Secret Number is:", secret_number)
+    print("Secret Number is:", User_Guess)
