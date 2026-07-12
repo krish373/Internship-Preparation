9d8763cd4b13a839f16d7a71116d707dbaaa120d
Krish Arora <223684188+krish373@users.noreply.github.com>
2026-07-13 01:51:38 +0530

    Improvement_in_Password_Generator

--- Python/Password_Generator.py
+++ Python/Password_Generator.py
@@ -1,4 +1,4 @@
-# Password Generator
+# Strong Password Generator with atleast one lowercase letter, one uppercase letter, one digit, and one special character in Python
 import random
 length = int(input("Enter the length of the password: "))
 lowercase = "abcdefghijklmnopqrstuvwxyz"
@@ -9,4 +9,7 @@ password_characters = lowercase + uppercase + digits + special_characters
 password = ""
 for i in range(length):
     password += random.choice(password_characters)
-print("Generated Password:", password)
+if(any(char.islower() for char in password) and any(char.isupper() for char in password) and any(char.isdigit() for char in password) and any(char in special_characters for char in password)):
+    print("Generated Password:", password)
+else:
+    print("Password does not meet the criteria. Please try again.")
